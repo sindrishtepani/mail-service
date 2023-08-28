9fa2430e10813b77c9b9b7aafef70640aa803154
sindri.shtepani@capgemini.com <sindri.shtepani@capgemini.com>
2023-08-27 23:31:04 -0600

    implemented mailing service

---
+++ cmd/api/handlers.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/tsawler/toolbox"
+)
+
+var tools toolbox.Tools
+
+func (app *Config) SendMail(w http.ResponseWriter, r *http.Request) {
+	type mailMessage struct {
+		From    string `json:"from"`
+		To      string `json:"to"`
+		Subject string `json:"subject"`
+		Message string `json:"message"`
+	}
+
+	var requestPayload mailMessage
+
+	err := tools.ReadJSON(w, r, &requestPayload)
+	if err != nil {
+		tools.ErrorJSON(w, err)
+		return
+	}
+
+	msg := Message{
+		From:    requestPayload.From,
+		To:      requestPayload.To,
+		Subject: requestPayload.Subject,
+		Data:    requestPayload.Message,
+	}
+
+	err = app.Mailer.SendSMTPMessage(msg)
+	if err != nil {
+		log.Println(err)
+		tools.ErrorJSON(w, err)
+		return
+	}
+
+	payload := toolbox.JSONResponse{
+		Error:   false,
+		Message: "send to " + requestPayload.To,
+	}
+
+	tools.WriteJSON(w, http.StatusAccepted, payload)
+}
